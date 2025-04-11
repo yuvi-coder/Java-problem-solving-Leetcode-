cf490fc1b03d34cda5473df97f31f3f96556fbe5
Yuvi_Developer <81914878+yuvi-coder@users.noreply.github.com>
2025-04-11 14:58:30 +0530

    Create Temp-Practice.java

---
+++ Temp-Practice.java
@@ -0,0 +1,11 @@
+class Main {
+    public static void main(String[] args) {
+        //System.out.println("Try programiz.pro");
+        strs = ["flower","flow","flight"]
+        String s="java";
+        char[] c=s.toCharArray();
+        for(char i:c){
+            System.out.println(i);
+        }
+    }
+}
